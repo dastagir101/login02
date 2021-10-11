c62501cbc48e47255bf0fbb07360f7d03e8de363
dastagir101 <72100991+dastagir101@users.noreply.github.com>
2021-10-11 22:07:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,96 @@
+if (process.env.NODE_ENV !=='production'){
+    require('dotenv').config()
+}
+
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const flash =require('express-flash')
+const session =require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+const passport = require('passport')
+initializePassport(
+    passport,
+    email =>users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+    )
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended:false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false
+
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+
+app.get('/', checkAuthenticated, (req,res) => {
+    res.render('index.ejs', {name: req.user.name})
+})
+
+app.get('/login',checkNotAuthenticated, (req,res) => {
+    res.render('login.ejs')
+})
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash:true
+}))
+
+app.get('/register',checkNotAuthenticated,  (req,res) => {
+    res.render('register.ejs')
+})
+app.post('/register', async (req,res) => {
+    console.log("In /register "+JSON.stringify(req.body))
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+        users.push({
+            id: Date.now().toString(),
+            name:req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        res.redirect('/login')
+    } catch(err) {
+        console.log("Error in register "+err.message)
+        res.redirect('/register')
+
+    }
+   console.log(users)
+})
+
+
+app.delete('/logout', (req,res) => {
+req.logOut()
+res.redirect('/login')
+
+})
+function checkAuthenticated(req,res, next){
+if (req.isAuthenticated()){
+    return next()
+}
+ res.redirect('/login')
+}
+
+function checkNotAuthenticated(req,res, next) {
+if (req.isAuthenticated()){
+    return res.redirect('/')
+}
+next()
+}
+
+
+
+app.listen(3000), function(){
+     console.log("server is started")
+}
